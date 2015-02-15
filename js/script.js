08784ccfe6a949d2f5769d67abbd2402ba212c1c
Joe <joe.ekiert@gmail.com>
2015-02-14 20:16:55 -0500

    mild reorganization of public/private methods, making init() private aaaaaand ... I dunno - OH YEAH, adding JocalStorage and lodash from my localhost library

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@
       $populateBtn = $('.populate');
 
 
-  // public methods
-  Pop.init = function () {
+  // private methods
+  var init = function () {
     // get this shit started!
     for (var i = 0; i < 24; i++) {
       hours.push(i);
@@ -24,33 +24,6 @@
     }
   };
 
-  Pop.populate = function () {
-    currentProblem.hour = randomArrValue(hours), 
-    currentProblem.minute = randomArrValue(minutes),
-    currentProblem.offset = randomOffset();
-
-    $hours.html(currentProblem.hour);
-    if (currentProblem.minute < 10) {
-      currentProblem.minute = "0" + currentProblem.minute;
-    }
-    $minutes.html(currentProblem.minute);
-    $offset.html(currentProblem.offset);
-
-    // JOE: looks like we got the beginnings of some clear() method
-    $answer.val('');
-    $container.removeClass('error');
-  };
-
-  Pop.submit = function () {
-    var arr = $answer.val().split(":");
-    // parse the answer data - add some error detection ... later
-    currentAnswer.hour = arr[0];
-    currentAnswer.minute = arr[1];
-
-    checkAnswer();
-  };
-
-  // Private methods
   var randomArrValue = function (arr) {
     var key = (Math.random() * 0x10000 | 0) % arr.length;
     return arr[key];
@@ -89,6 +62,33 @@
 
   };
 
+  // public methods
+  Pop.populate = function () {
+    currentProblem.hour = randomArrValue(hours), 
+    currentProblem.minute = randomArrValue(minutes),
+    currentProblem.offset = randomOffset();
+
+    $hours.html(currentProblem.hour);
+    if (currentProblem.minute < 10) {
+      currentProblem.minute = "0" + currentProblem.minute;
+    }
+    $minutes.html(currentProblem.minute);
+    $offset.html(currentProblem.offset);
+
+    // JOE: looks like we got the beginnings of some clear() method
+    $answer.val('');
+    $container.removeClass('error');
+  };
+
+  Pop.submit = function () {
+    var arr = $answer.val().split(":");
+    // parse the answer data - add some error detection ... later
+    currentAnswer.hour = arr[0];
+    currentAnswer.minute = arr[1];
+
+    checkAnswer();
+  };
+
   // event bindings
   $populateBtn.on('click', function () {
     Pop.populate();
@@ -98,7 +98,6 @@
     Pop.submit();
   });
 
-
-  Pop.init();
+  init();
 })( window.Pop = window.Pop || {}, jQuery);
 
