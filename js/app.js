88c4b6d47189d1a4cbdd529241514b5c92eeed10
Joe <joe.ekiert@gmail.com>
2015-02-21 01:47:42 -0800

    tightening up structure even more

--- js/app.js
+++ js/app.js
@@ -12,104 +12,103 @@ app.Defaults = (function () {
   }
 }());
 
-app.Problem = {};
-app.Problem.init = function () {
-  var AM = 'am',
-      PM = 'pm';
+app.Problem = {
+  new : function () {
+    var AM = 'am',
+        PM = 'pm';
 
-  var AMPMTEXT = {};
-  AMPMTEXT[AM] = ' in the morning.';
-  AMPMTEXT[PM] = ' in the afternoon.';
+    var AMPMTEXT = {};
+    AMPMTEXT[AM] = ' in the morning.';
+    AMPMTEXT[PM] = ' in the afternoon.';
 
-  var FAST = 'fast',
-      SLOW = 'slow';
+    var FAST = 'fast',
+        SLOW = 'slow';
 
-  var OFFSETTEXT = {};
-  OFFSETTEXT[FAST] = ' hour(s) fast.';
-  OFFSETTEXT[SLOW] = ' hour(s) slow.';
+    var OFFSETTEXT = {};
+    OFFSETTEXT[FAST] = ' hour(s) fast.';
+    OFFSETTEXT[SLOW] = ' hour(s) slow.';
 
-  this.hour = m.prop(app.utility.getRandomInt(4, 24));
-  this.minute = m.prop(app.utility.randomArrValue(app.Defaults.minutes));
-  this.offset = m.prop(app.utility.getRandomInt(0, 3));
+    this.hour = m.prop(app.utility.getRandomInt(4, 24));
+    this.minute = m.prop(app.utility.randomArrValue(app.Defaults.minutes));
+    this.offset = m.prop(app.utility.getRandomInt(0, 3));
 
-  if (this.offset() !== 0) {
-    this.offsetText = this.offset() > 0 ? m.prop(OFFSETTEXT[SLOW]) : m.prop(OFFSETTEXT[FAST]);
+    if (this.offset() !== 0) {
+      this.offsetText = this.offset() > 0 ? m.prop(OFFSETTEXT[SLOW]) : m.prop(OFFSETTEXT[FAST]);
+    }
+
+    if (this.hour() > 12) {
+      this.displayHour = m.prop(this.hour() - 12);
+      this.ampm = m.prop(PM);
+      this.ampmText = m.prop(AMPMTEXT[PM]);
+    }
+    else {
+      this.displayHour = m.prop(this.hour());
+      this.ampm = m.prop(AM); 
+      this.ampmText = m.prop(AMPMTEXT[AM]);
+    }
+
+    if (this.minute() < 10) {
+      this.displayMinute = m.prop("0" + this.minute());
+    }
+    else {
+      this.displayMinute = m.prop(this.minute());
+    }
   }
 
-  if (this.hour() > 12) {
-    this.displayHour = m.prop(this.hour() - 12);
-    this.ampm = m.prop(PM);
-    this.ampmText = m.prop(AMPMTEXT[PM]);
-  }
-  else {
-    this.displayHour = m.prop(this.hour());
-    this.ampm = m.prop(AM); 
-    this.ampmText = m.prop(AMPMTEXT[AM]);
-  }
+};
 
-  if (this.minute() < 10) {
-    this.displayMinute = m.prop("0" + this.minute());
-  }
-  else {
-    this.displayMinute = m.prop(this.minute());
+app.Answer = {
+  set : function (data) {
+    var arr = [];
+    if (data.answer) {
+      arr = data.answer.split(":")
+    }
+    else {
+      return { error: 'Incorrect format' };
+    }
+    this.hour = m.prop(arr[0]);
+    this.minute = m.prop(arr[1]);
   }
 };
 
-app.Answer = {};
-app.Answer.init = function (data) {
-  var arr = [];
-  if (data.answer) {
-    arr = data.answer.split(":")
+app.Clock = {
+  init : function () {
+    this.canvas = {};
+    this.clock = {};
+    this.hourHand = {};
+    this.minuteHand = {};
+  },
+  draw : function () {
+    //var hour_sign;
+    this.canvas = Raphael("clock", 200, 200);
+    this.clock = this.canvas.circle(100, 100, 95);
+
+    this.clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"});
+    for(i=0;i<12;i++){
+      var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
+      var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
+      var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
+      var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));  
+      //hour_sign = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+    }    
+    var pin = this.canvas.circle(100, 100, 5);
+    pin.attr("fill", "#000000");
+  },
+  update : function (problem) {
+    if (this.hourHand.remove) {
+      this.hourHand.remove();
+    }
+    if (this.minuteHand.remove) {
+      this.minuteHand.remove();
+    }
+
+    this.hourHand = this.canvas.path("M100 100L100 50");
+    this.hourHand.attr({stroke: "#444444", "stroke-width": 6});
+    this.minuteHand = this.canvas.path("M100 100L100 40");
+    this.minuteHand.attr({stroke: "#444444", "stroke-width": 4});
+    this.hourHand.rotate(30 * problem.displayHour() + (problem.minute() / 2.5), 100, 100);
+    this.minuteHand.rotate(6 * problem.minute(), 100, 100);
   }
-  else {
-    return { error: 'Incorrect format' };
-  }
-  this.hour = m.prop(arr[0]);
-  this.minute = m.prop(arr[1]);
-};
-
-app.Clock = {};
-app.Clock.init = function () {
-  this.canvas = {};
-  this.clock = {};
-  this.hourHand = {};
-  this.minuteHand = {};
-};
-
-// raphael clock methods
-app.Clock.drawClock = function () {
-  //var hour_sign;
-  this.canvas = Raphael("clock", 200, 200);
-  this.clock = this.canvas.circle(100, 100, 95);
-
-  this.clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"});
-  for(i=0;i<12;i++){
-    var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
-    var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
-    var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
-    var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));  
-    //hour_sign = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
-  }    
-  var pin = this.canvas.circle(100, 100, 5);
-  pin.attr("fill", "#000000");    
-
-  //this.updateClock()
-};
-
-app.Clock.updateClock = function (currentProblem) {
-  if (this.hourHand.remove) {
-    this.hourHand.remove();
-  }
-  if (this.minuteHand.remove) {
-    this.minuteHand.remove();
-  }
-
-  this.hourHand = this.canvas.path("M100 100L100 50");
-  this.hourHand.attr({stroke: "#444444", "stroke-width": 6});
-  this.minuteHand = this.canvas.path("M100 100L100 40");
-  this.minuteHand.attr({stroke: "#444444", "stroke-width": 4});
-  this.hourHand.rotate(30 * currentProblem.displayHour() + (currentProblem.minute() / 2.5), 100, 100);
-  this.minuteHand.rotate(6 * currentProblem.minute(), 100, 100);
 };
 
 app.utility = {};
@@ -125,8 +124,6 @@ app.controller = function () {
   var ctrl = this;
 
 
-
-  console.log("JOE: controller");
 };
 
 app.view = function (ctrl) {
